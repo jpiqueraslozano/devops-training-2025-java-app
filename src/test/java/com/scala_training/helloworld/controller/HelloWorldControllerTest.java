4a9f173d184aeeb18f85bef2c72c98de394de0e9
jpiqueraslozano <jpiqueraslozano@gmail.com>
2025-05-03 13:02:55 +0100

    cambiado test para que compruebe que es Hello Worldapt-get install git

--- src/test/java/com/scala_training/helloworld/controller/HelloWorldControllerTest.java
+++ src/test/java/com/scala_training/helloworld/controller/HelloWorldControllerTest.java
@@ -23,7 +23,7 @@ public class HelloWorldControllerTest {
 
         mockMvc.perform(get("/hello"))
                 .andExpect(status().isOk())
-                .andExpect(content().string("Hello World!"));
+                .andExpect(content().string("Hello World!1"));
     }
 
 
